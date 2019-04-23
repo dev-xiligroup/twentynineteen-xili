918266ed795de496c8a3fca2b1df8a8a53c6289e
dev-xiligroup <contact@xiligroup.com>
2019-04-23 11:53:33 +0200

    customize fixed

--- twentyseventeen-xili/inc/customizer.php
+++ twentyseventeen-xili/inc/customizer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Twenty Seventeen xili Customizer functionality
+ * Twenty Nineteen xili Customizer functionality
  *
  * @package WordPress
  * @subpackage Twenty_Seventeen
@@ -10,35 +10,36 @@
 /**
  * Add postMessage support for site title and description for the Customizer.
  *
- * @since Twenty Seventeen 1.0
+ * @since Twenty Nineteen 1.0
  *
  * @param WP_Customize_Manager $wp_customize Customizer object.
  */
-function twentyseventeen_xili_customize_register( $wp_customize ) {
+function twentynineteen_xili_customize_register( $wp_customize ) {
 	$wp_customize->add_section( 'texts_section' , array(
-					'title'       => __( 'Other texts section', 'twentyseventeen' ),
+					'title'       => __( 'Other texts section', 'twentynineteen' ),
 					'priority'    => 421
 	));
 
 	$wp_customize->add_setting( 'copyright', array(
-		'sanitize_callback' => 'twentyseventeenx_sanitize_text',
-		'default' => __('My company','twentyseventeen'),
+		'sanitize_callback' => 'twentynineteenxili_sanitize_text',
+		'default' => __('My company','twentynineteen'),
 		'transport' => 'postMessage',
-		) );
+		)
+	);
 
 	$wp_customize->add_control( 'copyright', array(
-				'label'    => __( 'Your copyright (footer)', 'twentyseventeen' ),
+				'label'    => __( 'Your copyright (footer)', 'twentynineteen' ),
 				'section'  => 'texts_section',
 				'settings' => 'copyright',
 				'priority'    => 1,
 		) );
 
 }
-add_action( 'customize_register', 'twentyseventeen_xili_customize_register', 12 ); // after parent
+add_action( 'customize_register', 'twentynineteen_xili_customize_register', 12 ); // after parent
 
 
-function twentyseventeenx_sanitize_text( $input ) {
-    return wp_kses_post( force_balance_tags( $input ) );
+function twentynineteenxili_sanitize_text( $input ) {
+	return wp_kses_post( force_balance_tags( $input ) );
 }
 
 /**
@@ -46,8 +47,8 @@ function twentyseventeenx_sanitize_text( $input ) {
  *
  * @since 1.2.0
  */
-function twentyseventeen_xili_customize_preview_js() {
-	wp_enqueue_script( 'twentyseventeen-xili-customize-preview', get_template_directory_uri() . '/assets/js/customize-preview.js', array( 'customize-preview' ), '20170824', true );
+function twentynineteen_xili_customize_preview_js() {
+	wp_enqueue_script( 'twentynineteen-xili-customize-preview', get_stylesheet_directory_uri() . '/js/customize-preview.js', array( 'customize-preview' ), '20170824', true );
+
 }
-add_action( 'customize_preview_init', 'twentyseventeen_xili_customize_preview_js' );
-?>
+add_action( 'customize_preview_init', 'twentynineteen_xili_customize_preview_js', 12 );
